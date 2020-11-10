ea00af8c3e014ec2839d92bd1dea6f2938c9774e
arrafeeahsan <arrafeeahsan@gmail.com>
2020-11-10 14:57:01 +0600

    ver.2

--- Lab6/Lab6/Library.cs
+++ Lab6/Lab6/Library.cs
@@ -7,6 +7,7 @@ namespace Lab6
     class Library
     {
         private string libName;
+
         public string LibName
         {
             get { return libName; }
@@ -14,10 +15,87 @@ namespace Lab6
         }
 
         private string libAddress;
+
         public string LibAddress
         {
             get { return libAddress; }
             set { libAddress = value; }
+        }
+        private Book[] ListOfBook;
+
+        private int totalBook;
+
+        public int TotalBook
+        {
+            get { return totalBook; }
+            set { totalBook = value; }
+        }
+
+        public Library()
+        {
+            ListOfBook = new Book[100];
+        }
+        public Library(string libName, string libAddress, int totalBook, Book[] listOfBook)
+        {
+            ListOfBook = new Book[100];
+            for (int i = 0; i < totalBook; i++)
+            {
+                this.ListOfBook[i] = listOfBook[i];
+            }
+            this.libName = libName;
+            this.libAddress = libAddress;
+            this.totalBook = totalBook;
+
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Library Name : " + libName);
+            Console.WriteLine("Library Address : " + libAddress);
+            Console.WriteLine("Total book : " + totalBook);
+        }
+
+        public void ShowAllBook()
+        {
+            for (int i = 0; i < totalBook; i++)
+            {
+                ListOfBook[i].ShowInfo();
+            }
+        }
+        public void AddNewBook(Book x)
+        {
+            ListOfBook[totalBook++] = x;
+
+
+        }
+        public void DeleteBook(Book x)
+        {
+            bool check = false;
+            for (int i = 0; i < totalBook; i++)
+            {
+                if (this.ListOfBook[i] == x)
+                {
+                    ListOfBook[i] = ListOfBook[i + 1];
+                    ListOfBook[i + 1] = x;
+                    check = true;
+                }
+            }
+            if (check) totalBook--;
+            else Console.WriteLine("Book Not Found In The List. Please try with write info");
+        }
+
+        public void AddNewBookCopy(Book x, int copy)
+        {
+            bool check = false;
+            for (int i = 0; i < totalBook; i++)
+            {
+                if (this.ListOfBook[i] == x)
+                {
+                    ListOfBook[i].BookCopy += copy;
+                    check = true;
+                }
+            }
+            if (!check) Console.WriteLine("Book Not Found In The List. Please try with write info");
+
         }
 
 
--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -6,7 +6,14 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book b1 = new Book("Megnathbodh", "Michel", "3453", "RomCom", 3);
+            Book b2 = new Book("Podda Nodir Majhi", "Manik Bondhopaddai", "5235", "Horror", 5);
+            Book b3 = new Book("Lal Shalu", "Abu Zafer", "6264", "Commedy", 2);
+            Library l = new Library("BookHouse", "Kuratoli", 3, new Book[] { b1, b2, b3 });
+            l.AddNewBook(new Book("Vindesh", "Abu Zafer", "6264", "Commedy", 2));
+            l.DeleteBook(b3);
+            l.AddNewBookCopy(b2, 3);
+            l.ShowAllBook();
         }
     }
 }
