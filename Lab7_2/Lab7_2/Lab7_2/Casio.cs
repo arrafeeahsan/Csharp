b06bfa30d31ea3f6c90fce913e999188a0141b21
arrafeeahsan <arrafeeahsan@gmail.com>
2020-11-25 11:57:51 +0600

    ver 1

---
+++ Lab7_2/Lab7_2/Lab7_2/Casio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Casio : Calculator, IBasicCalculator
+    {
+        public int division(int x, int y, int division)
+        {
+            return x + y;
+        }
+
+        public int multiplication(int x, int y, int multiplication)
+        {
+           
+        }
+
+        public int sub(int x, int y, int sub)
+        {
+            
+        }
+
+        public int sum(int x, int y, int sum)
+        {
+           
+        }
+    }
+}
